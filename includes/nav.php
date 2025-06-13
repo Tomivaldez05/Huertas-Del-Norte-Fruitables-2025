0c90df2826ab03c4a5fafffba765206bb9c94cc8
marialuzsalinasgit <marialuzsalinas629@gmail.com>
2025-06-13 11:31:25 -0300

    Detalles

--- includes/nav.php
+++ includes/nav.php
@@ -1,14 +1,17 @@
 <!-- includes/nav.php -->
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="navbar-brand-wrapper d-flex justify-content-center">
-    <a class="navbar-brand brand-logo" href="#"><img src="../assets/img/bg.jpg" alt="logo" /></a>
+    <!-- <a class="navbar-brand brand-logo" href="#">
+      <img src="../assets/img/LOGO.png" alt="Huertas del Norte" style="height: 50px;" />
+    </a> -->
+
     <a class="navbar-brand brand-logo-mini" href="#"><img src="../assets/img/avatar.jpg" alt="logo" /></a>
   </div>
   <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
     <ul class="navbar-nav navbar-nav-right">
       <li class="nav-item nav-profile dropdown">
         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-          <img src="../assets/img/mpLogo.png" alt="profile" />
+          <!-- <img src="../assets/img/mpLogo.png" alt="profile" /> -->
         </a>
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
           <a class="dropdown-item">
